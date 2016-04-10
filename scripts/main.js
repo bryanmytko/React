26a041a8cae03bfac62589214bc6598e9a746db9
Bryan Mytko <bryanmytko@gmail.com>
2016-04-09 23:14:46 -0400

    Add lesson 9

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var ReactRouter = require("react-router");
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 
 var createBrowserHistory = require("history/lib/createBrowserHistory");
 
@@ -82,9 +83,15 @@ var Inventory = React.createClass({
  */
 
 var StorePicker = React.createClass({
+  mixins: [History],
+  goToStore: function(e) {
+    e.preventDefault();
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, "/store/" + storeId);
+  },
   render: function(){
             return(
-              <form className="store-selector">
+              <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter a Store</h2>
                 <input type="text" ref="storeId" defaultValue={h.getFunName()} />
                 <input type="submit" />
