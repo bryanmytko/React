bef744c978f67cbd835eac2438f533a381f71bce
Bryan Mytko <bryanmytko@gmail.com>
2016-05-26 14:12:48 -0400

    Add lessong 15

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,11 @@ var createBrowserHistory = require("history/lib/createBrowserHistory");
 
 var h = require("./helpers.js");
 
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://catch-of-the-day-3f037.firebaseio.com');
+
+
+
 /*
  * App
  */
@@ -21,6 +26,12 @@ var App = React.createClass({
       fishes: {},
       order: {}
     }
+  },
+  componentDidMount: function(){
+    base.syncState(this.props.params.storeId + "/fishes", {
+      context: this,
+      state: 'fishes'
+    });
   },
   addToOrder: function(key){
     this.state.order[key] = this.state.order[key] + 1 || 1;
